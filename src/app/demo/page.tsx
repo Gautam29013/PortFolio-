53a609c043b41f07f377b0d284a9853732c573a8
Gautam29013 <jonygautam29013@gmail.com>
2026-08-25 14:39:23 +0530

    refactor: simplify Hero component structure and remove unused demo files and scripts

--- src/app/demo/page.tsx
+++
@@ -1,9 +0,0 @@
-import DemoOne from "@/components/ui/demo";
-
-export default function DemoPage() {
-  return (
-    <main className="min-h-screen bg-black">
-      <DemoOne />
-    </main>
-  );
-}
